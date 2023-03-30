08a0616dd3bc4d5989230957814b9462e0349ab5
Zukisa_Maza <same7nte@gmail.com>
2023-03-30 17:08:24 +0200

    2nd_5rd commit of the day

--- script.js
+++ script.js
@@ -87,19 +87,19 @@
 // }
 
 
-function submit() {
-    let num = document.querySelector('.number').value;
+// function submit() {
+//     let num = document.querySelector('.number').value;
 
-//check if the number is even
-if(num % 2 == 0) {
-    alert("The number is even.");
-}
+// //check if the number is even
+// if(num % 2 == 0) {
+//     alert("The number is even.");
+// }
 
-// if the number is odd
-else {
-    alert("The number is odd.");
-}
-    }
+// // if the number is odd
+// else {
+//     alert("The number is odd.");
+// }
+//     }
 
 // let num = 4;
 // let num2 = 3;
@@ -128,3 +128,34 @@ else {
 // } else {
 //     console.log("none of this is even nor odd.");
 // }
+
+// Array --> List
+// let numbers = [2,9,11,7,5,3,4,8,15,17,19,25,28,45]
+// console.log(numbers.length)
+// for (let i = 9; i >= 9; i++) {
+//         console.log(i)
+// }
+// console.log(numbers[0]);
+
+
+let numbers = [2, 9, 11, 3, 7, 5, 13, 4, 8, 15, 17, 19, 25, 28, 32, 45, 64, 72, 81, 99.]
+for (let i = 0; i <= numbers.length ; i+=1) {
+    if (numbers[i] % 2 == 0) {
+        console.log("Hello, " + numbers[i] + " is Even number");
+    } 
+}
+
+// let colors = ['White', 'Indigo', 'Light_Black'];
+// for (const color of colors){
+//     console.log(color);
+// }
+
+// let people = [
+//     {name: 'John', age: 23}, 
+//     {name: 'Andrew', age: 3}, 
+//     {name: 'Peter', age: 8}, 
+//     {name: 'Hanna', age: 14}, 
+//     {name: 'Adam', age: 37}];
+
+// let anyYouth = people.some(person => person.age >= 18);
+// console.log(anyYouth); // true
