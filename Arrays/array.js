13cae909fc8df9d964cb930538d29e37ef7f3ab3
Zukisa_Maza <same7nte@gmail.com>
2023-04-05 16:01:03 +0200

    3rd weeks work: Array

---
+++ Arrays/array.js
@@ -0,0 +1,19 @@
+
+// let myNumbers = [5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5];
+// let sum = 0;
+
+// for (let i = 0; i < myNumbers.length; i++ ) {
+//   sum += myNumbers[i];
+// }
+
+// console.log(sum)
+
+// multiplication 
+let myNumbs = [5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5];
+let product = 1;
+
+for (let i = 0; i < myNumbs.length; i++ ) {
+  product *= myNumbs[i];
+}
+
+console.log(product)
